389d7456edca38d7e0d3ff934efe88243ca8435d
binwoonki <vinwoonki98@gmail.com>
2024-09-10 21:21:39 +0900

    DiaryItem 기본 구조 작성

--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -1,5 +1,21 @@
+import { getEmotionImage } from "../util/get-emotion-image";
+import Button from "./Button";
+
 const DiaryItem = () => {
-  return <div>item</div>;
+  return (
+    <div className="DiaryItem">
+      <div className="img_section">
+        <img src={getEmotionImage(1)} />
+      </div>
+      <div className="info_section">
+        <div className="created_date">{new Date().toLocaleDateString()}</div>
+        <div className="content">일기 컨텐츠</div>
+      </div>
+      <div className="button_section">
+        <Button text={"수정하기"} />
+      </div>
+    </div>
+  );
 };
 
 export default DiaryItem;
