c99a9804acf8719c1aededa028055afe3c913db7
binwoonki <vinwoonki98@gmail.com>
2024-09-10 17:02:47 +0900

    라우트 컴포넌트 적용하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Home";
+import New from "./pages/Home";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={Home} />
+      <Route path="/diary" element={Diary} />
+      <Route path="/new" element={New} />
+    </Routes>
+  );
 }
 
 export default App;
