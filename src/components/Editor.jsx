8bb97198e84d7ee622e31f0446a43ea12e25733a
binwoonki <vinwoonki98@gmail.com>
2024-09-11 16:58:37 +0900

    Editor 컴포넌트 UI 작업 완료

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,7 +1,32 @@
+import Button from "./Button";
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 
+const emotionList = [
+  {
+    emotionId: 1,
+    emotionName: "완전 좋음",
+  },
+  {
+    emotionId: 2,
+    emotionName: "좋음",
+  },
+  {
+    emotionId: 3,
+    emotionName: "그럭저럭",
+  },
+  {
+    emotionId: 4,
+    emotionName: "나쁨",
+  },
+  {
+    emotionId: 5,
+    emotionName: "끔찍함",
+  },
+];
 const Editor = () => {
+  const emotionId = 1;
+
   return (
     <div className="Editor">
       <section className="date_section">
@@ -10,10 +35,24 @@ const Editor = () => {
       </section>
       <section className="emotion_section">
         <h4>오늘의 감정</h4>
-        <EmotionItem />
+        <div className="emotion_list_wrapper">
+          {emotionList.map((item) => (
+            <EmotionItem
+              key={item.emotionId}
+              {...item}
+              isSelected={item.emotionId === emotionId}
+            />
+          ))}
+        </div>
       </section>
-      <section className="content_section"></section>
-      <section className="button_section"></section>
+      <section className="content_section">
+        <h4>오늘의 일기</h4>
+        <textarea placeholder="오늘은 어땠나요?" />
+      </section>
+      <section className="button_section">
+        <Button text={"취소하기"} />
+        <Button text={"작성완료"} type={"POSITIVE"} />
+      </section>
     </div>
   );
 };
