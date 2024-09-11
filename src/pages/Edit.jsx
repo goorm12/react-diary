628c7da7da1a9f79b8994e7040756ff0092c31f4
binwoonki <vinwoonki98@gmail.com>
2024-09-11 18:39:28 +0900

    useNavigate를 사용하여 뒤로가기 버튼 클릭 시 뒤로가기 , 삭제하기 버튼 클릭 시 정말로 삭제할 것인지 물어본 후 확인 버튼 클릭 하면 Home 페이지로 이동 및 데이터 삭제 하기 기능 추가

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,15 +1,31 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+
 const Edit = () => {
+  const { onDelete } = useContext(DiaryDispatchContext);
   const params = useParams();
+  const nav = useNavigate();
+
+  const onClickDelete = () => {
+    if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
+      onDelete(params.id);
+      nav(-1, { replace: true });
+    }
+    return;
+  };
+
   return (
     <div>
       <Header
         title={"일기 수정하기"}
-        leftChild={<Button text={"< 뒤로가기"} />}
-        rightChild={<Button text={"삭제하기"} type={"NEGATIVE"} />}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로가기"} />}
+        rightChild={
+          <Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"} />
+        }
       />
       <Editor />
     </div>
