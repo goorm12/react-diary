5bbddfd64c3afdfbbc61821eb724fe6f44e27ff3
binwoonki <vinwoonki98@gmail.com>
2024-09-11 20:26:52 +0900

    Diary 페이지 Header 버튼 기능 구현 완성

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -2,15 +2,19 @@ import { useParams } from "react-router-dom";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Viewer from "../components/Viewer";
+import { useNavigate } from "react-router-dom";
 const Diary = () => {
   const params = useParams();
+  const nav = useNavigate();
 
   return (
     <div>
       <Header
         title={"yyyy-mm-dd 기록"}
-        leftChild={<Button text={"< 뒤로가기"} />}
-        rightChild={<Button text={"수정하기"} />}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로가기"} />}
+        rightChild={
+          <Button onClick={() => nav(`/edit/${params.id}`)} text={"수정하기"} />
+        }
       />
       <Viewer />
     </div>
