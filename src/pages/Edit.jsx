3d69b6bff32dea6e84c47129abfb26d518e01004
binwoonki <vinwoonki98@gmail.com>
2024-09-11 19:00:56 +0900

    잘 못된 주소(id)로 접근하면 alert 창으로 존재하지 않는 일기입니다. 안내 후 홈으로 이동하기

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -2,13 +2,28 @@ import { useParams, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
-import { useContext } from "react";
-import { DiaryDispatchContext } from "../App";
+import { useContext, useEffect, useState } from "react";
+import { DiaryDispatchContext, DiaryStateContext } from "../App";
 
 const Edit = () => {
   const { onDelete } = useContext(DiaryDispatchContext);
+  const data = useContext(DiaryStateContext);
   const params = useParams();
   const nav = useNavigate();
+  const [currentDiary, setCurrentDiary] = useState();
+
+  useEffect(() => {
+    const currentDiaryItem = data.find(
+      (item) => String(item.id) === String(params.id)
+    );
+
+    if (!currentDiaryItem) {
+      window.alert("존재하지 않는 일기입니다.");
+      nav("/", { replace: true });
+    }
+
+    setCurrentDiary(currentDiaryItem);
+  }, [params.id, data]);
 
   const onClickDelete = () => {
     if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
