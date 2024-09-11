596a417e2b0ab365ae670ab2932e943c7636bd0d
binwoonki <vinwoonki98@gmail.com>
2024-09-11 16:58:57 +0900

    EmotionItem 컴포넌트 작업 완료

--- src/components/EmotionItem.jsx
+++ src/components/EmotionItem.jsx
@@ -1,7 +1,17 @@
 import "./EmotionItem.css";
+import { getEmotionImage } from "../util/get-emotion-image";
 
-const EmotionItem = () => {
-  return <div>EmotionItem</div>;
+const EmotionItem = ({ emotionId, emotionName, isSelected }) => {
+  return (
+    <div
+      className={`EmotionItem ${
+        isSelected ? `EmotionItem_on_${emotionId}` : ""
+      }`}
+    >
+      <img className="emotion_img" src={getEmotionImage(emotionId)} />
+      <div className="emotion_name">{emotionName}</div>
+    </div>
+  );
 };
 
 export default EmotionItem;
